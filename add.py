c0a084a832fec05bc6b6833ff1a03a7dfef95141
Your Name <you@example.com>
2025-01-10 12:30:41 +0530

    first commit

---
+++ add.py
@@ -0,0 +1,11 @@
+#this program adds two numbers
+num1=1.5
+num2=6.3
+Add [D[D[D[D#Add two numbers 
+sum=num1+num2
+#Display the sum
+print('the sum of {}[D0} and {1} is {2}'.format(num1,num2,sum)
+
+
+
+rm
